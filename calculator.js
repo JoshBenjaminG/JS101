1b6cab4f13d554c463a7b9bc08a72a3338d1b828
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2025-07-30 22:54:05 -0500

    refactor

--- calculator.js
+++ calculator.js
@@ -1,40 +1,54 @@
-const readLine = require('readline-sync');
+const readline = require('readline-sync');
 
 function prompt(message) {
   console.log(message);
 }
 
+function invalidNumber(number) {
+  return Number.isNaN(Number(number));
+}
+
 // Get user input
 prompt("=> What is the first number?");
-let firstNumber = Number(readLine.question());
+let firstNumber = Number(readline.question());
+
+while (invalidNumber(firstNumber)) {
+  prompt("=> That's not a valid number.");
+  firstNumber = Number(readline.question());
+}
 
 prompt("=> What is the second number?");
-let secondNumber = Number(readLine.question());
+let secondNumber = Number(readline.question());
 
-prompt("=> What is the operation?");
-let operation = readLine.question();
+while (invalidNumber(secondNumber)) {
+  prompt("=> That's not a valid number.");
+  secondNumber = Number(readline.question());
+}
+
+prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+let operation = readline.question();
 
 function calculator() {
   let result;
 
   switch (operation) {
-    case '+':
+    case '1':
       result = Number(firstNumber) + Number(secondNumber);
       break;
-    case '-':
+    case '2':
       result = Number(firstNumber) - Number(secondNumber);
       break;
-    case '*':
+    case '3':
       result = Number(firstNumber) * Number(secondNumber);
       break;
-    case '/':
+    case '4':
       result = Number(firstNumber) / Number(secondNumber);
       break;
     default:
       result = "Invalid operation";
   }
 
-  console.log(`=> ${firstNumber} ${operation} ${secondNumber} is ${result}`);
+  console.log(`=> The output is ${result}`);
 }
 
 
