2ae22d818efc93719589543a9ee43c792bf6431d
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2025-09-27 16:11:00 -0500

    add easy problems

---
+++ easyproblems.js
@@ -0,0 +1,1 @@
+easyproblems.js
---
+++ example.js
@@ -0,0 +1,10 @@
+function oddoreven(num) {
+  if (num % 2 === 0) {
+    console.log("It's even");
+  } else {
+    console.log("It's odd");
+  }
+}
+
+oddoreven(2);
+oddoreven(3);
